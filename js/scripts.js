e4a1c47005faf934795a34bc2115e07f2037ae67
MIKE <mike@mike-Aspire-5517.(none)>
2014-10-04 16:58:05 -0700

    Version Desktop terminada falta el breakpoint de 480

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	console.log("Hola Softtek ;)");
+	
+	$('.slide').first().addClass('active');
+	$('.slide').hide();    
+	$('.active').show();
+
+	$('#next').click(function(){
+
+		$('.active').removeClass('active').addClass('oldActive');    
+		if ( $('.oldActive').is(':last-child')) {
+			$('.slide').first().addClass('active');
+		}
+		else{
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut();
+		$('.active').fadeIn();
+		
+	});
+
+	$('#back').click(function(){
+		$('.active').removeClass('active').addClass('oldActive');    
+		if ( $('.oldActive').is(':first-child')) {
+			$('.slide').last().addClass('active');
+		}
+		else{
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut();
+		$('.active').fadeIn();
+	});
+
+	function auto(){
+		interval = setInterval(function(){
+			$('#next').click();
+		},2000);
+	}
+	auto();
+});
+
